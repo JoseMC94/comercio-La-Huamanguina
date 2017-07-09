343048b7a2935cc09790a575c1115dcc31cae94f
Jose Antonio Maldonado Candela <josephcand@gmail.com>
2017-07-08 21:32:11 -0500

    Primer Cambio

--- katana/index.php
+++ katana/index.php
@@ -2,9 +2,6 @@
 // el archivo autoload inicializa todos lo archivos necesarios para que el framework funcione
 include "core/autoload.php";
 
-
 // cargamos el modulo iniciar.
 $lb = new Lb();
 $lb->loadModule("index");
-
-?>
